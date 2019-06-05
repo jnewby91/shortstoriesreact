7d7812220d4c9fa82833c1f2db0a432a7985db0a
Justin  Newby <jnewby91@gmail.com>
2019-06-05 07:27:31 -0400

    makes changes to config.js file

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 //export default URL for MONGO database
-export const API_BASE_URL = process.env.REACT_APP_API_BAASE_URL || 'http://localhost:8080'; 
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080'; 
